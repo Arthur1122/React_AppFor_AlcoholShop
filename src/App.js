9ed6ee675ec3ad40f757bee9c7060cb63487c8a3
Artur Petrosyan <arm.arturpetrosyan@gmail.com>
2019-10-30 12:47:00 +0400

    Add bootstrap and design table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="continer-fluid">
+        <div className="row">
+          <div className="col-sm-3"></div>
+          <div className="col-sm-6 heght">
+            <table className="table ">
+              <tbody>
+                <thead className="thead-dark">
+                  <tr>
+                    <th>Name</th>
+                    <th>Type</th>
+                    <th>Year</th>
+                    <th>Made in</th>
+                    <th>Degree</th>
+                    <th>Pulled</th>
+                  </tr>
+                </thead>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="col-sm-3"></div>
+      </div>
     </div>
   );
 }
