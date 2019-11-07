8d51281a0895a888312a3d912206a0879b0c55fb
Artur Petrosyan <arm.arturpetrosyan@gmail.com>
2019-11-07 22:10:49 +0400

    UI snack bar after buttun

--- src/Components/AddDeepModal.js
+++ src/Components/AddDeepModal.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { Modal, Button, Row, Col, Form } from "react-bootstrap";
 import axios from "axios";
 
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+
 export default class AddDeepModal extends Component {
   constructor(props) {
     super(props);
@@ -11,9 +14,19 @@ export default class AddDeepModal extends Component {
       model_Year_of_alcohol: "",
       model_Alcohol_degree: "",
       model_Pulled_from: "",
-      model_Made_In: ""
+      model_Made_In: "",
+
+      snackbaropen: false,
+      snackbarmsg: ""
     };
+
+    this.submitHandler = this.submitHandler.bind(this); // for snackbar
   }
+
+  //snack bar close
+  snackbarClose = event => {
+    this.setState({ snackbaropen: false });
+  };
 
   //When input data it will be changed here
   handleChange = event => {
@@ -27,10 +40,12 @@ export default class AddDeepModal extends Component {
     axios
       .post("http://localhost:1111/api/alcohols", this.state)
       .then(response => {
+        this.setState({ snackbaropen: true, snackbarmsg: "Added succesfuly" });
         console.log(response);
-        alert("Added succesfuly");
+        //alert("Added succesfuly");
       })
       .catch(error => {
+        this.setState({ snackbaropen: true, snackbarmsg: "Failed" });
         console.log(error);
       });
   };
@@ -45,106 +60,125 @@ export default class AddDeepModal extends Component {
       model_Pulled_from
     } = this.state;
     return (
-      <Modal
-        {...this.props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Add Alchohol
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div className="container">
-            {" "}
-            {/*Here Form for Add alcohol */}
-            <Row>
-              <Col sm={6}>
-                <Form onSubmit={this.submitHandler}>
-                  <Form.Group controlId="model_Name">
-                    <Form.Label>Alcohol Name</Form.Label>
-                    <Form.Control
-                      type="text"
-                      name="model_Name"
-                      required
-                      placeholder="Input Alcohol name"
-                      value={model_Name}
-                      onChange={this.handleChange}
-                    />
-                  </Form.Group>
-                  <Form.Group controlId="model_Alcohol_type">
-                    <Form.Label>Alcohol type</Form.Label>
-                    <Form.Control
-                      type="text"
-                      name="model_Alcohol_type"
-                      required
-                      placeholder="Input Alcohol type"
-                      value={model_Alcohol_type}
-                      onChange={this.handleChange}
-                    />
-                  </Form.Group>
-                  <Form.Group controlId="model_Year_of_alcohol">
-                    <Form.Label>Alcohol old</Form.Label>
-                    <Form.Control
-                      type="number"
-                      name="model_Year_of_alcohol"
-                      required
-                      placeholder="Input Alcohol old"
-                      value={model_Year_of_alcohol}
-                      onChange={this.handleChange}
-                    />
-                  </Form.Group>
-                  <Form.Group controlId="model_Alcohol_degree">
-                    <Form.Label>Alcohol degree</Form.Label>
-                    <Form.Control
-                      type="number"
-                      name="model_Alcohol_degree"
-                      required
-                      placeholder="Input Alcohol degree"
-                      value={model_Alcohol_degree}
-                      onChange={this.handleChange}
-                    />
-                  </Form.Group>
-                  <Form.Group controlId="model_Made_In">
-                    <Form.Label>Made In</Form.Label>
-                    <Form.Control
-                      type="text"
-                      name="model_Made_In"
-                      required
-                      placeholder="Input Made in"
-                      value={model_Made_In}
-                      onChange={this.handleChange}
-                    />
-                  </Form.Group>
-                  <Form.Group controlId="model_Pulled_from">
-                    <Form.Label>Pulled from</Form.Label>
-                    <Form.Control
-                      type="text"
-                      name="model_Pulled_from"
-                      required
-                      placeholder="Input from puuld ?"
-                      value={model_Pulled_from}
-                      onChange={this.handleChange}
-                    />
-                  </Form.Group>
-                  <Form.Group>
-                    <Button variant="primary" type="submit">
-                      Add alcohol
-                    </Button>
-                  </Form.Group>
-                </Form>
-              </Col>
-            </Row>
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="danger" onClick={this.props.onHide}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      <div className="container">
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }} //snack bar code
+          open={this.state.snackbaropen}
+          autoHideDuration={3000}
+          onClose={this.snackbarClose}
+          message={<span id="message-id">{this.state.snackbarmsg}</span>}
+          action={[
+            <IconButton
+              key="close"
+              arial-label="Close"
+              color="inherit"
+              onClick={this.snackbarClose}
+            >
+              X
+            </IconButton>
+          ]}
+        />
+        <Modal
+          {...this.props}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Add Alchohol
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="container">
+              {" "}
+              {/*Here Form for Add alcohol */}
+              <Row>
+                <Col sm={6}>
+                  <Form onSubmit={this.submitHandler}>
+                    <Form.Group controlId="model_Name">
+                      <Form.Label>Alcohol Name</Form.Label>
+                      <Form.Control
+                        type="text"
+                        name="model_Name"
+                        required
+                        placeholder="Input Alcohol name"
+                        value={model_Name}
+                        onChange={this.handleChange}
+                      />
+                    </Form.Group>
+                    <Form.Group controlId="model_Alcohol_type">
+                      <Form.Label>Alcohol type</Form.Label>
+                      <Form.Control
+                        type="text"
+                        name="model_Alcohol_type"
+                        required
+                        placeholder="Input Alcohol type"
+                        value={model_Alcohol_type}
+                        onChange={this.handleChange}
+                      />
+                    </Form.Group>
+                    <Form.Group controlId="model_Year_of_alcohol">
+                      <Form.Label>Alcohol old</Form.Label>
+                      <Form.Control
+                        type="number"
+                        name="model_Year_of_alcohol"
+                        required
+                        placeholder="Input Alcohol old"
+                        value={model_Year_of_alcohol}
+                        onChange={this.handleChange}
+                      />
+                    </Form.Group>
+                    <Form.Group controlId="model_Alcohol_degree">
+                      <Form.Label>Alcohol degree</Form.Label>
+                      <Form.Control
+                        type="number"
+                        name="model_Alcohol_degree"
+                        required
+                        placeholder="Input Alcohol degree"
+                        value={model_Alcohol_degree}
+                        onChange={this.handleChange}
+                      />
+                    </Form.Group>
+                    <Form.Group controlId="model_Made_In">
+                      <Form.Label>Made In</Form.Label>
+                      <Form.Control
+                        type="text"
+                        name="model_Made_In"
+                        required
+                        placeholder="Input Made in"
+                        value={model_Made_In}
+                        onChange={this.handleChange}
+                      />
+                    </Form.Group>
+                    <Form.Group controlId="model_Pulled_from">
+                      <Form.Label>Pulled from</Form.Label>
+                      <Form.Control
+                        type="text"
+                        name="model_Pulled_from"
+                        required
+                        placeholder="Input from puuld ?"
+                        value={model_Pulled_from}
+                        onChange={this.handleChange}
+                      />
+                    </Form.Group>
+                    <Form.Group>
+                      <Button variant="primary" type="submit">
+                        Add alcohol
+                      </Button>
+                    </Form.Group>
+                  </Form>
+                </Col>
+              </Row>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="danger" onClick={this.props.onHide}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
     );
   }
 }
